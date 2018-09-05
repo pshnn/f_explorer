cfd03d8184b1fb81452c7f794c7c87ac6362aabb
ipv <pshnn481@gmail.com>
2018-09-05 10:59:41 +0300

    Update ApplicationController
    - add ApplicationController#client method;
    - add ApplicationController#current_user method;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
+# ApplicationController
 class ApplicationController < ActionController::Base
+  private
+
+  def current_user
+    @current_user ||= client.get_current_account
+  end
+
+  def client
+    @client ||= DropboxApi::Client.new
+  end
 end
