3ce8563aa2c21506ca812eb1b7b3a1e1be778c99
ipv <pshnn481@gmail.com>
2018-09-07 13:58:59 +0300

    Prettify size for DropboxFile

--- app/models/dropbox_file.rb
+++ app/models/dropbox_file.rb
@@ -2,12 +2,12 @@
 
 # DropboxFile
 class DropboxFile
-  attr_reader :name, :path, :size, :type
+  attr_reader :name, :path, :type
 
   def initialize(params = {})
     @name = params.fetch 'name', nil
     @path = params.fetch 'path_lower', nil
-    @size = params.fetch 'size', nil
+    @filesize = params.fetch 'size', nil
     @client_last_modified = params.fetch 'client_modified', nil
     @server_last_modified = params.fetch 'server_modified', nil
     @type = params.fetch '.tag', nil
@@ -17,7 +17,11 @@ class DropboxFile
     client_last_modified.to_datetime.strftime '%c'
   end
 
+  def size
+    Filesize.new("#{filesize} B").pretty
+  end
+
   private
 
-  attr_reader :client_last_modified, :server_last_modified
+  attr_reader :client_last_modified, :server_last_modified, :filesize
 end
